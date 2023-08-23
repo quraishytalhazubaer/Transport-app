4e5d7fbd65638f5f6e85e1bae990cc4c5ee6918f
quraishytalhazubaer <108470925+quraishytalhazubaer@users.noreply.github.com>
2023-08-23 18:29:03 +0600

    Update models.py

--- myProjectFinal/transport/models.py
+++ myProjectFinal/transport/models.py
@@ -364,3 +364,13 @@ class Train_Book(models.Model):
 
     def __str__(self):
         return f"Train from {self.From} to {self.To}"
+
+
+class Conv(models.Model):
+    #user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_message = models.TextField()
+    response = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user_message}"
